b9f46f21e49ba4d5f30515b7fad55b8cb9686464
quarnstric <nishusainialwar@gmail.com>
2023-11-02 17:13:19 +0530

    flake8 error fixed

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,11 @@
+"""
+Test for the Django admin modifications
+"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin.."""
+    pass
