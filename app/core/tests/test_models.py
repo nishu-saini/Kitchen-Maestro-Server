d0280d04d140ae6053fdff298fa13139e002d54e
quarnstric <nishusainialwar@gmail.com>
2023-11-02 16:35:33 +0530

    implemented valid email and superuser functionality

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,7 +14,7 @@ class ModelTests(TestCase):
         email = 'test@example.com'
         password = 'testpass123'
 
-        user = get_user_model().objects.create(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
@@ -33,5 +33,22 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in sample_emails:
-            user = get_user_model().objects.create(email=email, password='sample123')
+            user = get_user_model().objects.create_user(email=email, password='sample123')
             self.assertEqual(user.email, expected)
+
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+
+    def test_create_superuser(self):
+        """Test creating a superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
