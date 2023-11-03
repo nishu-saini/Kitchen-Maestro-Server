c819cb965a19fe20cf0e359e444b584036c4f12f
quarnstric <nishusainialwar@gmail.com>
2023-11-03 18:19:41 +0530

    added user app and created testcases for it

---
+++ app/user/urls.py
@@ -0,0 +1,13 @@
+"""
+URL mappings for the user API
+"""
+from django.urls import path
+from user.views import CreateUserView
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', CreateUserView.as_view(), name='create')
+]
+
