c03d5704cac44d49b522c0fe7da09bc8f805cd9c
quarnstric <nishusainialwar@gmail.com>
2023-11-03 22:35:25 +0530

    Implemented authentication class to user view

--- app/user/urls.py
+++ app/user/urls.py
@@ -2,12 +2,16 @@
 URL mappings for the user API
 """
 from django.urls import path
-from user.views import CreateUserView
+from user.views import (
+    CreateUserView,
+    CreateTokenView
+)
 
 
 app_name = 'user'
 
 urlpatterns = [
-    path('create/', CreateUserView.as_view(), name='create')
+    path('create/', CreateUserView.as_view(), name='create'),
+    path('token/', CreateTokenView.as_view(), name='token'),
 ]
 
